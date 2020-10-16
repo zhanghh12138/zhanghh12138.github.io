14018a8a3745417b55466e271ed3d1ec2a2bf723
zhanghh12138 <49849337+zhanghh12138@users.noreply.github.com>
2020-10-17 01:28:23 +0800

    update

--- zip/zip.js
+++ zip/zip.js
@@ -1,14 +1,4 @@
 function a () {
-	var str = ['name.txt','zip.js']
 	htmlobj=$.ajax({url:"zip/name.txt",async:false});
 	q = htmlobj.responseText.split("\n");
-	q.pop()
-	for(var i = 0;i < str.length ;i++) {
-		for(var m = 0;m < q.length;m++) {
-			if(q[m].trim() == str[i].trim()) {
-				q.splice(m,1)
-				m--
-			}
-		}
-	}
 }
